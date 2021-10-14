271c27f8cdcbd98864c72deb626757de428097d4
prithivirajasingh <prithivirajasingh@gmail.com>
2021-10-14 11:51:18 -0700

    import os, shutil, zipfile added

--- file_handling.py
+++ file_handling.py
@@ -29,3 +29,44 @@ f = open("demofile2.txt", "a")
 f.seek(10)
 f.write("Now the file has more content!")
 f.close()
+
+import os
+if os.path.exists("file.txt"):
+  os.remove("file.txt") # same as os.unlink
+  os.unlink('file.txt') # same as os.remove
+else:
+  print("The file does not exist")
+os.rmdir("myfolder") # Note: You can only remove empty folders.
+cmd = 'echo LINUX COMMAND HERE' # cmd = 'mv -n "' + premod + '" "' + postmod + '"'
+os.system(cmd)
+os.chdir('/tmp/')
+for folder, subfolders, files in os.walk(target):
+  pass
+for filename in os.listdir():
+    if filename.endswith('.txt'):
+        print(filename)
+
+import shutil
+shutil.copy('source_FILE', 'destination_FOLDER') # shutil.copy('C:\\spam.txt', 'C:\\delicious')
+shutil.copy('source_FILE', 'destination_FILE')  # shutil.copy('eggs.txt', 'C:\\delicious\\eggs2.txt') # Copy and rename
+shutil.copytree('source_FOLDER', 'destination_FOLDER')  # shutil.copytree('C:\\bacon', 'C:\\bacon_backup')
+shutil.move('source_FILE', 'destination_FOLDER') # shutil.move('C:\\bacon.txt', 'C:\\eggs')  
+shutil.move('source_FILE', 'destination_FILE')  # shutil.move('C:\\bacon.txt', 'C:\\eggs\\new_bacon.txt') # Move and rename
+# FileNotFoundError # If any folder before destination folder does not exist
+shutil.rmtree(path)  # will remove the folder at path, and all files and folders it contains will also be deleted.
+
+import zipfile
+# Extract ZIP
+os.chdir('C:\\')    # move to the folder with example.zip
+exampleZip = zipfile.ZipFile('example.zip')
+exampleZip.namelist()
+exampleZip.extractall()
+exampleZip.extractall('C:\\ delicious')  # the code would extract the files from example.zip into a newly created C:\delicious folder
+exampleZip.extract('spam.txt', 'C:\\some\\new\\folders')
+exampleZip.close()
+# Compress ZIP
+newZip = zipfile.ZipFile('new.zip', 'w')
+newZip.write('spam.txt', compress_type=zipfile.ZIP_DEFLATED)
+newZip.close()
+
+
