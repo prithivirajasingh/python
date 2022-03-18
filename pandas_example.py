6eacb2c4b755448cdf27cff089bd8d4c55800be7
prithivirajasingh <prithivirajasingh@gmail.com>
2022-03-17 22:11:33 -0700

    Update pandas_example.py

--- pandas_example.py
+++ pandas_example.py
@@ -9,3 +9,21 @@ print(data.shape[0])    # returns row count
 print(data.shape[0])    # returns column count
 exit()
 
+
+productsURL = "products.csv"
+products= pd.read_csv(productsURL)
+
+products.index += 1
+products = products.rename_axis('ID')
+# left_aligned_products = products.style.set_properties(**{'text-align': 'left'})
+# left_aligned_products
+# print(type(products))
+# sort = products.sort_values(products.columns[0])
+print(tabulate(products, headers='keys', showindex=True, tablefmt='psql', numalign='left', floatfmt=".2f"))
+# print(sort)
+# print(products.to_string(header=False))
+# print("{:<15} {:<15}".format(products.iloc[:, 0].to_string(index=False, header=False), products.iloc[:, 1].to_string(index=False, header=False)))
+# print("{:<15} {:<15} {:<15} {:.2f}".format(products.to_string(header=False)))
+print(products.iat[1, 0])
+# print(products.shape[0])
+exit()
