a18ec7aa49b0664ac2b531f9657e9ab412372358
prithivirajasingh <prithivirajasingh@gmail.com>
2022-05-20 14:04:47 -0700

    Update shell_python_integration.py

--- shell_python_integration.py
+++ shell_python_integration.py
@@ -1,7 +1,22 @@
 import subprocess as sub
 
 encoding = 'utf-8'
-cmd = sub.Popen( ['ls', '-a', '-l'], stdout = sub.PIPE, stderr=sub.STDOUT)
-for line in cmd.stdout:
-    print(line.rstrip(b'\n').decode(encoding))
+
+winId = sub.Popen( ['xdotool', 'getactivewindow'], stdout = sub.PIPE, stderr=sub.STDOUT)
+winIdOut = []
+for line in winId.stdout:
+    winIdOut.append(line.rstrip(b'\n').decode(encoding))
+
+print(winIdOut)
+command = ['xwininfo', '-id', winIdOut[0]]
+print(command)
+
+winInfo = sub.Popen(command, stdout = sub.PIPE, stderr=sub.STDOUT)
+winInfoOut = []
+for line in winInfo.stdout:
+    winInfoOut.append(line.rstrip(b'\n').decode(encoding))
+    # print(line.rstrip(b'\n').decode(encoding))
+
+print('\n'.join(winInfoOut))
+
 
