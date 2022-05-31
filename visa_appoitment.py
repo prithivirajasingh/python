2685ed5c3dd5660235b88f0e1e346a4b750b73d1
prithivirajasingh <prithivirajasingh@gmail.com>
2022-05-31 02:57:39 -0700

    Create visa_appoitment.py

---
+++ visa_appoitment.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+import pyautogui
+import time
+import random
+import vlc
+import subprocess
+encoding = 'utf-8'
+
+playsound('/home/prithivi/visa/scam.mp3')
+print('test')
+time.sleep(5)
+exit(0)
+
+# time.sleep(5)
+command = subprocess.Popen( ['xdotool', 'getactivewindow', 'getwindowname'], stdout = subprocess.PIPE, stderr=subprocess.STDOUT)
+window_name = ''
+for line in command.stdout:
+    window_name = window_name + (line.rstrip(b'\n').decode(encoding))
+print(window_name)
+# if not 'cgifederal.secure.force.com/scheduleappointment - Google Chrome' in window_name:
+#     print('Error: Window name doesn't match. Program will now exit with error code 1.')
+#     exit(1)
+# print('0')
+# exit(0)
+time.sleep(random.randint(1,5) * 60)
+pyautogui.press('f5')
+time.sleep(2)
+pyautogui.press('enter')
+time.sleep(2)
+command = subprocess.Popen( ['xfce4-screenshooter', '-f', '-s', '/home/prithivi/visa/screenshot.png'], stdout = subprocess.PIPE, stderr=subprocess.STDOUT)
+screen_image = ''
+for line in command.stdout:
+    screen_image = screen_image + (line.decode(encoding))
+# print(screen_image)
+time.sleep(2)
+command = subprocess.Popen( ['tesseract', '--dpi', '96', '/home/prithivi/visa/screenshot.png', '-'], stdout = subprocess.PIPE, stderr=subprocess.STDOUT)
+screen_text = ''
+for line in command.stdout:
+    screen_text = screen_text + (line.decode(encoding))
+print(screen_text)
+if '2022' in screen_text and 'November' in screen_text:
+    print('Inside if')
+if '2022' in screen_text and 'December' in screen_text:
+    print('Inside if')
+if 'January' in screen_text:
+    print('Inside if')
+if 'maximum' in screen_text.lower():
+    print('Error: Reaching maximum attempts. Program will now exit with error code 2.')
+    exit(2)
+
+
